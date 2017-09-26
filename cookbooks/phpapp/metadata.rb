d17ff8207583056bb41db4551cd3f4cc8e0c58dd
George <george@xminds.com>
2017-09-26 18:22:29 +0530

    Test

---
+++ cookbooks/phpapp/metadata.rb
@@ -0,0 +1,16 @@
+name             'phpapp'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures phpapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+#%w{ apt }.each do |pkg|
+#  depends pkg
+#end
+
+#%w{ ubuntu }.each do |os|
+#  supports os
+#end
+depends 'apt'
